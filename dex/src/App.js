4395be8ec8e89177803ef6d83ea15236ed312ff2
Saurabh Dabas <developerdabas@gmail.com>
2022-08-02 13:40:46 +0530

    Add LongTermSwap

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Swap from './components/Swap';
+import LongTermSwap from './components/LongTermSwap';
 
 function App() {
   return (
     <div className="App">
-      <Swap/>
+      {/* <Swap/> */}
+      <LongTermSwap/>
     </div>
   );
 }
