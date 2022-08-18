9b32b7d4d829e1e26bbd1eda841e8c483db263fd
Kunal Sangwan <kunalsangwan1@gmail.com>
2022-08-18 11:51:32 +0530

    Fix modal issue

--- dex/src/components/Modal.jsx
+++ dex/src/components/Modal.jsx
@@ -1,77 +1,98 @@
-import React,{useState} from 'react';
-import { FAUCET_TOKEN_ADDRESS, MATIC_TOKEN_ADDRESS } from '../utils';
+import React, { useState } from "react";
+import { FAUCET_TOKEN_ADDRESS, MATIC_TOKEN_ADDRESS } from "../utils";
 
-
-const Modal = ({display,setDisplay,setTokenA,setTokenB,selectToken}) => {
-
+const Modal = ({ display, setDisplay, setTokenA, setTokenB, selectToken }) => {
   const tokenDetails = [
     {
-      symbol : "MATIC",
-      image : "/ethereum.png",
-      address: {MATIC_TOKEN_ADDRESS}
+      symbol: "MATIC",
+      image: "/ethereum.png",
+      address: { MATIC_TOKEN_ADDRESS },
     },
     {
-      symbol : "FAUCET",
-      image : "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
-      address: {FAUCET_TOKEN_ADDRESS}
+      symbol: "FAUCET",
+      image:
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+      address: { FAUCET_TOKEN_ADDRESS },
     },
-
   ];
   // Handle Select Token Modal display
   const handleModalClose = () => {
     setDisplay(!display);
-  }
+  };
   const handleTokenSelection = (event) => {
     const token = event.currentTarget;
-    if(selectToken){
+    if (selectToken === "2") {
       setTokenB({
-        address:token.children[2].innerHTML,
-        symbol:token.children[1].innerHTML,
-        image :token.children[0].src.slice(21,token.length),
-      })
-    } else {
-      setTokenA({ 
-        address:token.children[2].innerHTML,
-        symbol:token.children[1].innerHTML,
-        image :token.children[0].src.slice(21,token.length)
-      })
+        address: token.children[2].innerHTML,
+        symbol: token.children[1].innerHTML,
+        image: token.children[0].src.slice(21, token.length),
+      });
+    } else if (selectToken === "1") {
+      setTokenA({
+        address: token.children[2].innerHTML,
+        symbol: token.children[1].innerHTML,
+        image: token.children[0].src.slice(21, token.length),
+      });
     }
-  }
+  };
 
   return (
     <>
-    {display ? <div className="modal__wrapper">
-      <div className="modal__container">
-        <div className="modal__heading">Select a token</div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="" onClick={handleModalClose}>
-          <line x1="18" y1="6" x2="6" y2="18"></line>
-          <line x1="6" y1="6" x2="18" y2="18"></line>
-        </svg>
-      </div>
-      <div className="modal__token-list">
-        <div className="modal__token" onClick={handleTokenSelection}>
-          <img className="modal__token-img" alt="ETH logo" src="/ethereum.png" style={{marginRight:'8px', width:'20px'}}/>
-          <div className="modal__token-symbol">Faucet</div>
-          <p style={{display: "none"}}>FAUCET:{FAUCET_TOKEN_ADDRESS}</p>
-
-        
-        </div>
-        <div className="modal__token" onClick={handleTokenSelection}>
-          <img className="modal__token-img" alt="DAI logo" src="/dai.png" style={{marginRight:'8px', width:'20px'}}/>
-          <div className="modal__token-symbol">Matic</div>
-          <p style={{display: "none"}}>MATIC:{MATIC_TOKEN_ADDRESS}</p>
-        </div>
-        {/* <div className="modal__token" onClick={handleTokenSelection}>
+      {display ? (
+        <div className="modal__wrapper">
+          <div className="modal__container">
+            <div className="modal__heading">Select a token</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className=""
+              onClick={handleModalClose}
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          </div>
+          <div className="modal__token-list">
+            <div className="modal__token" onClick={handleTokenSelection}>
+              <img
+                className="modal__token-img"
+                alt="ETH logo"
+                src="/ethereum.png"
+                style={{ marginRight: "8px", width: "20px" }}
+              />
+              <div className="modal__token-symbol">Faucet</div>
+              <p style={{ display: "none" }}>FAUCET:{FAUCET_TOKEN_ADDRESS}</p>
+            </div>
+            <div className="modal__token" onClick={handleTokenSelection}>
+              <img
+                className="modal__token-img"
+                alt="DAI logo"
+                src="/dai.png"
+                style={{ marginRight: "8px", width: "20px" }}
+              />
+              <div className="modal__token-symbol">Matic</div>
+              <p style={{ display: "none" }}>MATIC:{MATIC_TOKEN_ADDRESS}</p>
+            </div>
+            {/* <div className="modal__token" onClick={handleTokenSelection}>
           <img className="modal__token-img" alt="DAI logo" src="/dai.png" style={{marginRight:'8px', width:'20px'}}/>
           <div className="modal__token-symbol">USDC</div>
           <p style={{display: "none"}}>USDC Address</p>
 
         </div> */}
-      </div>
-    </div>  : ""}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
-    
-  )
-}
+  );
+};
 
 export default Modal;
--- dex/src/components/Swap.jsx
+++ dex/src/components/Swap.jsx
@@ -9,17 +9,17 @@ import TokenListFetching from "../Helpers/TokenListFetching";
 const Swap = () => {
   // Handle Select Token Modal display
   const [display, setDisplay] = useState(false);
-  const [selectToken, setSelectToken] = useState(false);
+  const [selectToken, setSelectToken] = useState("0");
   // useContext To Get swapAmount From InputField
-  const {swapAmount, setSwapAmount}= useContext(ShortSwapContext)
-  const {srcAddress, setSrcAddress} = useContext(ShortSwapContext);
-  const {destAddress, setDestAddress} = useContext(ShortSwapContext);
+  const { swapAmount, setSwapAmount } = useContext(ShortSwapContext);
+  const { srcAddress, setSrcAddress } = useContext(ShortSwapContext);
+  const { destAddress, setDestAddress } = useContext(ShortSwapContext);
 
   const handleDisplay = (event) => {
-    console.log(event.currentTarget.className.includes("token"));
+    console.log(event.currentTarget.id);
     // console.log(tokenA.address);
 
-    setSelectToken(event.currentTarget.className.includes("token"));
+    setSelectToken(event.currentTarget.id);
     setDestAddress(tokenB.address);
     setSrcAddress(tokenA.address);
     // console.log(tokenB.address);
@@ -27,18 +27,18 @@ const Swap = () => {
   };
   // const CMC_TOKEN_LIST = TokenListFetching();
   // console.log(CMC_TOKEN_LIST);
-   
+
   // Set Default Token A & Token B
   const [tokenA, setTokenA] = useState({
     symbol: "Faucet",
     image: "/ethereum.png",
     // address:"Token A Adress"
-
   });
 
   const [tokenB, setTokenB] = useState({
     symbol: "Matic",
-    image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+    image:
+      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
     // address : "Token B Adress",
   });
 
@@ -66,10 +66,15 @@ const Swap = () => {
             type="text"
             placeholder="0.0"
             value={swapAmount}
-            onChange={(e)=> setSwapAmount(e.target.value)}
+            onChange={(e) => setSwapAmount(e.target.value)}
           />
-          <button className="btn currency-select" onClick={handleDisplay}  
-            value={srcAddress} onChange={(e)=> setSrcAddress(e.target.value)}>
+          <button
+            className="btn currency-select"
+            onClick={handleDisplay}
+            value={srcAddress}
+            id="1"
+            onChange={(e) => setSrcAddress(e.target.value)}
+          >
             <span className="spn-currency">
               <div className="currency">
                 <img
@@ -107,57 +112,39 @@ const Swap = () => {
             onChange={handleSecondary}
           />
 
-          {!selectToken ? (
-            <button className="btn token-select" onClick={handleDisplay} >
-              <span className="spn-select-token">
-                <div className="">
-                  <span className="select-container">Select a token</span>
-                </div>
+          <button
+            className="btn currency-select"
+            onClick={handleDisplay}
+            value={destAddress}
+            id="2"
+            onChange={(e) => setDestAddress(e.target.value)}
+          >
+            <span className="spn-currency">
+              <div className="currency">
+                <img
+                  className="tokenImage"
+                  src={tokenB.image}
+                  style={{ marginRight: "0.5rem" }}
+                  alt="token-image"
+                />
+                <span className="token-container">{tokenB.symbol}</span>
                 <svg
-                  width="12"
-                  height="7"
-                  viewBox="0 0 12 7"
-                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="sc-33m4yg-8 jsUfgJ"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="sc-w04zhs-16 lfEMTx"
                 >
-                  <path
-                    d="M0.97168 1L6.20532 6L11.439 1"
-                    stroke="#AEAEAE"
-                  ></path>
+                  <polyline points="6 9 12 15 18 9"></polyline>
                 </svg>
-              </span>
-            </button>
-          ) : (
-            <button className="btn currency-select" onClick={handleDisplay} value={destAddress} 
-            onChange={(e)=> setDestAddress(e.target.value)}>
-              <span className="spn-currency">
-                <div className="currency">
-                  <img
-                    className="tokenImage"
-                    src={tokenB.image}
-                    style={{ marginRight: "0.5rem" }}
-                    alt="token-image"
-                  />
-                  <span className="token-container">{tokenB.symbol}</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="sc-w04zhs-16 lfEMTx"
-                  >
-                    <polyline points="6 9 12 15 18 9"></polyline>
-                  </svg>
-                </div>
-              </span>
-            </button>
-          )}
+              </div>
+            </span>
+          </button>
         </div>
       </form>
       {display ? (
