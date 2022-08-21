8f158e9db5b641f315ebd2e369629aea47a46d94
subash <imsuubash@gmail.com>
2022-08-21 15:04:35 +0545

    add input module css

--- dex/src/components/Input.jsx
+++ dex/src/components/Input.jsx
@@ -1,81 +1,94 @@
 import Modal from "./Modal";
+import styles from "../css/Input.module.css";
+import classnames from "classnames";
 
-const Input = ({
-  id,
-  onChange,
-  input,
-  imgSrc,
-  symbol,
-  handleDisplay,
-  selectToken,
-  display,
-  setDisplay,
-  setTokenA,
-  setTokenB,
-}) => {
+const Input = (props) => {
+  const {
+    id,
+    onChange,
+    input,
+    imgSrc,
+    symbol,
+    handleDisplay,
+    selectToken,
+    display,
+    setDisplay,
+    setTokenA,
+    setTokenB,
+  } = props;
+
   return (
     <>
-      <div className="textInput">
-        <input
-          className="textField"
-          type="text"
-          placeholder="0.0"
-          value={input}
-          onChange={onChange}
-        />
-        {!selectToken ? (
-          <button className="btn token-select" onClick={handleDisplay}>
-            <span className="spn-select-token">
-              <div>
-                <span className="select-container">Select a token</span>
-              </div>
-              <svg
-                width="12"
-                height="7"
-                viewBox="0 0 12 7"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="sc-33m4yg-8 jsUfgJ"
-              >
-                <path d="M0.97168 1L6.20532 6L11.439 1" stroke="#AEAEAE"></path>
-              </svg>
-            </span>
-          </button>
-        ) : (
-          <button
-            className="btn currency-select"
-            onClick={handleDisplay}
-            id={id}
-          >
-            <span className="spn-currency">
-              <div className="currency">
-                <img
-                  className="tokenImage"
-                  src={imgSrc}
-                  style={{ marginRight: "0.5rem" }}
-                  alt="token-image"
-                />
-                <span className="token-container">{symbol}</span>
+      <div className={styles.textInput}>
+        <div className={styles.inputSelectContainer}>
+          <input
+            className={styles.textField}
+            type="text"
+            placeholder="0.0"
+            value={input}
+            onChange={onChange}
+          />
+          {!selectToken ? (
+            <button
+              className={classnames(styles.btn, styles.tokenSelect)}
+              onClick={handleDisplay}
+            >
+              <span className={styles.spnSelectToken}>
+                <div>
+                  <span className={styles.selectContainer}>Select a token</span>
+                </div>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
+                  width="12"
+                  height="7"
+                  viewBox="0 0 12 7"
                   fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="sc-w04zhs-16 lfEMTx"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="sc-33m4yg-8 jsUfgJ"
                 >
-                  <polyline points="6 9 12 15 18 9"></polyline>
+                  <path
+                    d="M0.97168 1L6.20532 6L11.439 1"
+                    stroke="#FFFFFF"
+                  ></path>
                 </svg>
-              </div>
-            </span>
-          </button>
-        )}
+              </span>
+            </button>
+          ) : (
+            <button
+              className={classnames(styles.btn, styles.currencySelect)}
+              onClick={handleDisplay}
+              id={id}
+            >
+              <span className={styles.spnCurrency}>
+                <div className={styles.currency}>
+                  <img
+                    className={styles.tokenImage}
+                    src={imgSrc}
+                    alt="token-image"
+                  />
+                  <p className={styles.tokenContainer}>{symbol}</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="sc-w04zhs-16 lfEMTx"
+                  >
+                    <polyline points="6 9 12 15 18 9"></polyline>
+                  </svg>
+                </div>
+              </span>
+            </button>
+          )}
+        </div>
+        <div className={styles.balance}></div>
       </div>
-      {display ? (
+
+      {display && (
         <Modal
           display={display}
           setDisplay={setDisplay}
@@ -83,8 +96,6 @@ const Input = ({
           setTokenA={setTokenA}
           setTokenB={setTokenB}
         />
-      ) : (
-        ""
       )}
     </>
   );
