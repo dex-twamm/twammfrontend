9f1941558ddd969e135dbce691bfd3578a67e978
subash <imsuubash@gmail.com>
2022-08-21 13:26:23 +0545

    add shortswap module css

--- dex/src/pages/ShortSwap.jsx
+++ dex/src/pages/ShortSwap.jsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import Swap from '../components/Swap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
+import styles from "../css/ShortSwap.module.css";
+import classnames from "classnames"
 
 const ShortSwap = ({tokenSymbol,tokenImage,connectWallet, buttonText}) => {
   return (
-    <div className='main-body'>
-      <div className="swap">
-        <div className="swapOptions">
-          <a className="textLink" href="/">Swap</a>
+    <div className={styles.mainBody}>
+      <div className={styles.swap}>
+        <div className={styles.swapOptions}>
+          <a className={styles.textLink} href="/">Swap</a>
           <FontAwesomeIcon icon={faGear} />
         </div>
       </div>
       <Swap tokenSymbol={tokenSymbol} tokenImage={tokenImage} />
-        <button className="btn btn-connect" onClick={connectWallet}>{buttonText}</button>
+      <button className={classnames(styles.btn, styles.btnConnect)} onClick={connectWallet}>
+        {buttonText}
+      </button>
     </div>
   )
 }
