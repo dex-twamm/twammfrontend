f70ff45d692995778a9aefbb76d70251db81bcd0
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-09-08 14:09:36 +0545

    Navigation Bug, Add Liquidity Validation, Model Items, UI refined

--- dex/src/components/Liquidity/RemoveLiquidity.jsx
+++ dex/src/components/Liquidity/RemoveLiquidity.jsx
@@ -18,131 +18,126 @@ const RemoveLiquidity = ({ showRemoveLiquidity }) => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.mainBody}>
-				<div className={styles.modalContainer}>
-					<div className={styles.modalHeading}>
-						<FontAwesomeIcon
-							onClick={() => showRemoveLiquidity(false)}
-							className={styles.icon}
-							icon={faArrowLeft}
-						/>
-						<span>Remove Liquidity</span>
-						<FontAwesomeIcon
-							className={styles.icon}
-							icon={faGear}
-						/>
-					</div>
+				<div className={styles.heading}>
+					<FontAwesomeIcon
+						onClick={() => showRemoveLiquidity(false)}
+						className={styles.icon}
+						icon={faArrowLeft}
+					/>
+					<span>Remove Liquidity</span>
+					<FontAwesomeIcon className={styles.icon} icon={faGear} />
+				</div>
 
-					<div className={styles.tokenRangeContainer}>
-						<div className={styles.token}>
-							<div className={styles.tokenImages}>
-								<img
-									className={styles.tokenImage}
-									src='https://seeklogo.com/images/E/ethereum-logo-EC6CDBA45B-seeklogo.com.png'
-									alt='#'
-									width={24}
-								/>
-								<img
-									className={styles.tokenImage}
-									src='https://seeklogo.com/images/U/uniswap-uni-logo-7B6173C76E-seeklogo.com.png'
-									alt='#'
-									width={24}
-								/>
-							</div>
-							<span>UNI/ETH</span>
+				<div className={styles.tokenRangeContainer}>
+					<div className={styles.token}>
+						<div className={styles.tokenImages}>
+							<img
+								className={styles.tokenImage}
+								src='https://seeklogo.com/images/E/ethereum-logo-EC6CDBA45B-seeklogo.com.png'
+								alt='#'
+								width={24}
+							/>
+							<img
+								className={styles.tokenImage}
+								src='https://seeklogo.com/images/U/uniswap-uni-logo-7B6173C76E-seeklogo.com.png'
+								alt='#'
+								width={24}
+							/>
 						</div>
-						<div className={styles.range}>
-							<div className={styles.status}></div>
-							In range
-						</div>
+						<span>UNI/ETH</span>
 					</div>
+					<div className={styles.range}>
+						<div className={styles.status}></div>
+						In range
+					</div>
+				</div>
 
-					<div className={styles.amountContainer}>
-						<h4 className={styles.amount}>Amount</h4>
-						<div className={styles.percentages}>
-							<span className={styles.percentageRange}>
-								{rangeValue}%
+				<div className={styles.amountContainer}>
+					<h4 className={styles.amount}>Amount</h4>
+					<div className={styles.percentages}>
+						<span className={styles.percentageRange}>
+							{rangeValue}%
+						</span>
+						<div className={styles.percentageBtns}>
+							<span
+								onClick={() => setRangeValue(25)}
+								className={styles.percentage}
+							>
+								25%
 							</span>
-							<div className={styles.percentageBtns}>
-								<span
-									onClick={() => setRangeValue(25)}
-									className={styles.percentage}
-								>
-									25%
-								</span>
-								<span
-									onClick={() => setRangeValue(50)}
-									className={styles.percentage}
-								>
-									50%
-								</span>
-								<span
-									onClick={() => setRangeValue(75)}
-									className={styles.percentage}
-								>
-									75%
-								</span>
-								<span
-									onClick={() => setRangeValue(100)}
-									className={styles.percentage}
-								>
-									Max
-								</span>
-							</div>
-						</div>
-						<input
-							className={styles.inputRange}
-							type='range'
-							max={100}
-							min={0}
-							value={rangeValue}
-							onChange={e => setRangeValue(e.target.value)}
-						/>
-					</div>
-
-					<div className={styles.pooled}>
-						<div className={styles.pooledToken}>
-							<div>Pooled UNI:</div>
-							<div className={styles.pooledTokenIcon}>
-								0.12437
-								<img
-									src='https://seeklogo.com/images/E/ethereum-logo-EC6CDBA45B-seeklogo.com.png'
-									alt='#'
-									width={18}
-								/>
-							</div>
-						</div>
-						<div className={styles.pooledToken}>
-							<div>Pooled ETH:</div>
-							<div className={styles.pooledTokenIcon}>
-								0.99951
-								<img
-									src='https://seeklogo.com/images/U/uniswap-uni-logo-7B6173C76E-seeklogo.com.png'
-									alt='#'
-									width={18}
-								/>
-							</div>
+							<span
+								onClick={() => setRangeValue(50)}
+								className={styles.percentage}
+							>
+								50%
+							</span>
+							<span
+								onClick={() => setRangeValue(75)}
+								className={styles.percentage}
+							>
+								75%
+							</span>
+							<span
+								onClick={() => setRangeValue(100)}
+								className={styles.percentage}
+							>
+								Max
+							</span>
 						</div>
 					</div>
+					<input
+						className={styles.inputRange}
+						type='range'
+						max={100}
+						min={0}
+						value={rangeValue}
+						onChange={e => setRangeValue(e.target.value)}
+					/>
+				</div>
 
-					<div className={styles.collect}>
-						<span>Collect as WETH</span>
-						<button
-							onClick={() => setButtonEnabled(state => !state)}
-							className={styles.collectBtn}
-						>
-							<div
-								className={classNames(
-									styles.circle,
-									buttonEnabled && styles.true
-								)}
-							></div>
-						</button>
+				<div className={styles.pooled}>
+					<div className={styles.pooledToken}>
+						<div>Pooled UNI:</div>
+						<div className={styles.pooledTokenIcon}>
+							0.12437
+							<img
+								src='https://seeklogo.com/images/E/ethereum-logo-EC6CDBA45B-seeklogo.com.png'
+								alt='#'
+								width={18}
+							/>
+						</div>
 					</div>
+					<div className={styles.pooledToken}>
+						<div>Pooled ETH:</div>
+						<div className={styles.pooledTokenIcon}>
+							0.99951
+							<img
+								src='https://seeklogo.com/images/U/uniswap-uni-logo-7B6173C76E-seeklogo.com.png'
+								alt='#'
+								width={18}
+							/>
+						</div>
+					</div>
+				</div>
 
-					<button onClick={clickHandler} className={styles.removeBtn}>
-						Remove
+				<div className={styles.collect}>
+					<span>Collect as WETH</span>
+					<button
+						onClick={() => setButtonEnabled(state => !state)}
+						className={styles.collectBtn}
+					>
+						<div
+							className={classNames(
+								styles.circle,
+								buttonEnabled && styles.true
+							)}
+						></div>
 					</button>
 				</div>
+
+				<button onClick={clickHandler} className={styles.removeBtn}>
+					Remove
+				</button>
 			</div>
 		</div>
 	);
--- dex/src/components/Modal.jsx
+++ dex/src/components/Modal.jsx
@@ -1,101 +1,150 @@
-import React, { useState } from "react";
-import { useContext } from "react";
-import { ShortSwapContext } from "../providers";
-import { FAUCET_TOKEN_ADDRESS, MATIC_TOKEN_ADDRESS } from "../utils";
-import styles from "../css/Modal.module.css";
+import React, { useContext, useEffect, useState } from 'react';
+import styles from '../css/Modal.module.css';
+import { LongSwapContext, ShortSwapContext } from '../providers';
+import { FAUCET_TOKEN_ADDRESS, MATIC_TOKEN_ADDRESS } from '../utils';
 
 const Modal = ({ display, setDisplay, setTokenA, setTokenB, tokenDetails }) => {
-  // useContext To Retrieve The Source and Destination Address of The Token
-  const { setSrcAddress, setDestAddress, selectToken, setEthBalance } =
-    useContext(ShortSwapContext);
+	// useContext To Retrieve The Source and Destination Address of The Token
+	const { setSrcAddress, setDestAddress, selectToken, setEthBalance } =
+		useContext(ShortSwapContext);
 
-  // Handle Modal Close
-  const handleModalClose = () => {
-    setDisplay(!display);
-  };
+	const { tokenA, tokenB } = useContext(LongSwapContext);
 
-  // Handle Select Token Modal display
-  const handleTokenSelection = (event) => {
-    console.log("TokenSelected", selectToken);
-    const token = event.currentTarget;
-    console.log("Modal:Handle", token.children[2].innerHTML);
-    if (selectToken === "1") {
-      setEthBalance(parseFloat(token.children[3].innerHTML).toFixed(2));
-      setSrcAddress(token.children[2].innerHTML);
-      setTokenA({
-        symbol: token.children[1].innerHTML,
-        image: token.children[0].src.slice(21, token.length),
-        balance: token.children[3].innerHTML,
-      });
-    } else if (selectToken === "2") {
-      setDestAddress(token.children[2].innerHTML);
-      setTokenB({
-        symbol: token.children[1].innerHTML,
-        image: token.children[0].src.slice(21, token.length),
-        balance: token.children[3].innerHTML,
-      });
-    }
-    handleModalClose();
-  };
+	// Handle Modal Close
+	const handleModalClose = () => {
+		setDisplay(!display);
+	};
 
-  // Mapping The Token Details Objects
-  const tokenList = tokenDetails.map((token) => {
-    return (
-      <div
-        className={styles.modalToken}
-        key={token.symbol}
-        onClick={handleTokenSelection}
-      >
-        <img
-          className={styles.modalTokenImg}
-          alt="ETH logo"
-          src={token.image}
-          style={{ width: "25px" }}
-        />
-        <p>{token.name}</p>
-        <p className={styles.tokenAddress} style={{ display: "none" }}>
-          {token.address}
-        </p>
-        <p className={styles.comingSoon}>
-          {parseFloat(token.balance).toFixed(2)}
-        </p>
-        {token.type === "coming_soon" && (
-          <div className={styles.comingSoon}>
-            <span>coming soon...</span>
-          </div>
-        )}
-      </div>
-    );
-  });
+	// Handle Select Token Modal display
+	const handleTokenSelection = event => {
+		console.log('TokenSelected Prabin', selectToken);
+		const token = event.currentTarget;
+		console.log('Modal:Handle', token.children[2].innerHTML);
+		if (selectToken === '1') {
+			setEthBalance(parseFloat(token.children[3].innerHTML).toFixed(2));
+			setSrcAddress(token.children[2].innerHTML);
+			setTokenA({
+				symbol: token.children[1].innerHTML,
+				image: token.children[0].src.slice(21, token.length),
+				balance: token.children[3].innerHTML,
+				tokenIsSet: true,
+			});
+		} else if (selectToken === '2') {
+			setDestAddress(token.children[2].innerHTML);
+			setTokenB({
+				symbol: token.children[1].innerHTML,
+				image: token.children[0].src.slice(21, token.length),
+				balance: token.children[3].innerHTML,
+				tokenIsSet: true,
+			});
+		}
+		handleModalClose();
+	};
 
-  return (
-    display && (
-      <div className={styles.modalWrapper}>
-        <div className={styles.container}>
-          <div className={styles.modalContainer}>
-            <div className={styles.modalHeading}>Select a token</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className=""
-              onClick={handleModalClose}
-            >
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-          </div>
-          <div className={styles.modalTokenList}>{tokenList}</div>
-        </div>
-      </div>
-    )
-  );
+	// Mapping The Token Details Objects - Subham
+	// const tokenList = tokenDetails.map(token => {
+	// 	return (
+	// 		<div
+	// 			className={styles.modalToken}
+	// 			key={token.symbol}
+	// 			onClick={handleTokenSelection}
+	// 		>
+	// 			<img
+	// 				className={styles.modalTokenImg}
+	// 				alt='ETH logo'
+	// 				src={token.image}
+	// 				style={{ width: '25px' }}
+	// 			/>
+	// 			<p>{token.name}</p>
+	// 			<p className={styles.tokenAddress} style={{ display: 'none' }}>
+	// 				{token.address}
+	// 			</p>
+	// 			<p className={styles.comingSoon}>
+	// 				{parseFloat(token.balance).toFixed(2)}
+	// 			</p>
+	// 			{token.type === 'coming_soon' && (
+	// 				<div className={styles.comingSoon}>
+	// 					<span>coming soon...</span>
+	// 				</div>
+	// 			)}
+	// 		</div>
+	// 	);
+	// });
+
+	let tokensList;
+	let tokensDetail = tokenDetails;
+	if (selectToken === '2')
+		tokensDetail = tokenDetails.filter(
+			token => token.name !== tokenA.symbol
+		);
+
+	tokensList = tokensDetail.map(token => {
+		return (
+			<div
+				className={styles.modalToken}
+				key={token.symbol}
+				onClick={handleTokenSelection}
+			>
+				<img
+					className={styles.modalTokenImg}
+					alt='ETH logo'
+					src={token.image}
+					style={{ width: '25px' }}
+				/>
+				<p>{token.name}</p>
+				<p className={styles.tokenAddress} style={{ display: 'none' }}>
+					{token.address}
+				</p>
+				<p className={styles.comingSoon}>
+					{parseFloat(token.balance).toFixed(2)}
+				</p>
+				{token.type === 'coming_soon' && (
+					<div className={styles.comingSoon}>
+						<span>coming soon...</span>
+					</div>
+				)}
+			</div>
+		);
+	});
+
+	return (
+		display && (
+			<div
+				onClick={() => {
+					setDisplay(false);
+				}}
+				className={styles.modalWrapper}
+			>
+				<div
+					onClick={e => e.stopPropagation()}
+					className={styles.container}
+				>
+					<div className={styles.modalContainer}>
+						<div className={styles.modalHeading}>
+							Select a token
+						</div>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							className=''
+							onClick={handleModalClose}
+						>
+							<line x1='18' y1='6' x2='6' y2='18'></line>
+							<line x1='6' y1='6' x2='18' y2='18'></line>
+						</svg>
+					</div>
+					<div className={styles.modalTokenList}>{tokensList}</div>
+				</div>
+			</div>
+		)
+	);
 };
 
 export default Modal;
