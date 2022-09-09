f41751f8d136e0fc364b9ce9cc36cea453bc598a
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-09-09 11:39:30 +0545

    Added new Open Sans font

--- dex/src/index.js
+++ dex/src/index.js
@@ -1,19 +1,19 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import AllProviders from "./providers";
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import AllProviders from './providers';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <AllProviders>
-      <Router>
-        <App />
-      </Router>
-    </AllProviders>
-  </StrictMode>
+	<StrictMode>
+		<AllProviders>
+			<Router>
+				<App />
+			</Router>
+		</AllProviders>
+	</StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
