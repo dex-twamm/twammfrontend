ae170143ca31076f02ee2604cbf85b8588a9619c
Safal <mr.safaladh@gmail.com>
2023-01-04 11:19:30 +0545

    Removed unused file and code

--- dex/src/components/Liquidity/index.js
+++ dex/src/components/Liquidity/index.js
@@ -1,3 +1,1 @@
-export { AddLiquidity } from './AddLiquidity';
-export { LiquidityPools } from './LiquidityPools';
-export { RemoveLiquidity } from './RemoveLiquidity';
+export { LiquidityPools } from "./LiquidityPools";
--- dex/src/pages/LiquidityPage.jsx
+++ dex/src/pages/LiquidityPage.jsx
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
-import {
-  AddLiquidity,
-  LiquidityPools,
-  RemoveLiquidity,
-} from "../components/Liquidity";
+import React from "react";
+import { LiquidityPools } from "../components/Liquidity";
 
 const LiquidityPage = () => {
-  const [showRemoveLiquidity, setShowRemoveLiquidity] = useState(false);
-  const [showAddLiquidity, setShowAddLiquidity] = useState(false);
-
-  let liquidityMarkup = (
-    <LiquidityPools
-      showRemoveLiquidity={setShowRemoveLiquidity}
-      showAddLiquidity={setShowAddLiquidity}
-    />
+  return (
+    <div>
+      <LiquidityPools />
+    </div>
   );
-
-  if (showAddLiquidity) {
-    liquidityMarkup = <AddLiquidity showAddLiquidity={setShowAddLiquidity} />;
-  } else if (showRemoveLiquidity)
-    liquidityMarkup = (
-      <RemoveLiquidity showRemoveLiquidity={setShowRemoveLiquidity} />
-    );
-
-  return <div>{liquidityMarkup}</div>;
 };
 
 export default LiquidityPage;
