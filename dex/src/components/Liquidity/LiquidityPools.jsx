9e4ca350b9f9117bea8501f7b6a0ee1bea08c9bd
Safal <mr.safaladh@gmail.com>
2023-01-12 15:16:33 +0545

    Liquidity showed when wallet is not connected as well

--- dex/src/components/Liquidity/LiquidityPools.jsx
+++ dex/src/components/Liquidity/LiquidityPools.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import styles from "../../css/LiquidityPool.module.css";
 import { useContext } from "react";
 import { ShortSwapContext } from "../../providers";
-import CircularProgressBar from "../alerts/CircularProgressBar";
 import Tabs from "../Tabs";
 import {
   getPoolFees,
   getPoolId,
-  getPoolTokens,
   getPoolTokenSymbol,
 } from "../../utils/poolUtils";
 import { UIContext } from "../../providers/context/UIProvider";
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Skeleton, Typography } from "@mui/material";
+import { SpaRounded, SpaTwoTone } from "@mui/icons-material";
 
 const LiquidityPools = () => {
   const { LPTokenBalance, loading, isWalletConnected } =
@@ -21,123 +20,115 @@ const LiquidityPools = () => {
   return (
     <Box className={styles.rootBox}>
       <Tabs />
-      {!isWalletConnected ? (
-        <Box className={styles.containerBox}>
-          <Typography className={styles.styledTypography}>
-            Connect your wallet to view your Pools.
-          </Typography>
-        </Box>
-      ) : !loading ? (
-        <>
-          {" "}
-          <Box
-            className={styles.headingBox}
-            sx={{
-              alignItems: {
-                xs: "flex-start",
-                sm: "center",
-                md: "center",
-                lg: "center",
-              },
-              justifyContent: {
-                xs: "center",
-                sm: "space-between",
-                md: "space-between",
-              },
-              flexDirection: { xs: "column", sm: "row", md: "row" },
-            }}
-          >
-            <Typography className={styles.poolTypography}>Pools</Typography>
-          </Box>
-          <Box
-            className={styles.contentBox}
-            sx={{
-              flexDirection: { xs: "row-reverse", sm: "row" },
-              justifyContent: { xs: "flex-end", sm: "flex-end" },
-            }}
-          >
-            <Box className={styles.insideContentBox}>
-              <Box className={styles.styledBoxOne}>
-                <Box className={styles.styledBoxTwo}>
-                  <Box className={styles.styledBoxThree}>
-                    <Box className={styles.styledBoxFour}>
-                      <Box className={styles.styledBoxFive}>
-                        <Avatar
-                          className={styles.styledAvatarOne}
-                          alt="Testv4"
-                          src="Testv4.jpeg"
-                        />
-                        <Avatar
-                          className={styles.styledAvatarTwo}
-                          sizes="small"
-                          alt="Faucet"
-                          src="ethereum.png"
-                        />
-                      </Box>
-
-                      <span
-                        className={styles.tokensTypography}
-                        sx={{ fontSize: { xs: 16 } }}
-                      >
-                        {`${getPoolTokenSymbol(selectedNetwork)?.[0]} / ${
-                          getPoolTokenSymbol(selectedNetwork)?.[1]
-                        }`}
-                      </span>
-                      <span className={styles.feeTypography}>
-                        {getPoolFees(selectedNetwork)}
-                      </span>
-                    </Box>
+      <Box
+        className={styles.headingBox}
+        sx={{
+          alignItems: {
+            xs: "flex-start",
+            sm: "center",
+            md: "center",
+            lg: "center",
+          },
+          justifyContent: {
+            xs: "center",
+            sm: "space-between",
+            md: "space-between",
+          },
+          flexDirection: { xs: "column", sm: "row", md: "row" },
+        }}
+      >
+        <span className={styles.poolTypography}>Pools</span>
+      </Box>
+      <Box
+        className={styles.contentBox}
+        sx={{
+          flexDirection: { xs: "row-reverse", sm: "row" },
+          justifyContent: { xs: "flex-end", sm: "flex-end" },
+        }}
+      >
+        <Box className={styles.insideContentBox}>
+          <Box className={styles.styledBoxOne}>
+            <Box className={styles.styledBoxTwo}>
+              <Box className={styles.styledBoxThree}>
+                <Box className={styles.styledBoxFour}>
+                  <Box className={styles.styledBoxFive}>
+                    <Avatar
+                      className={styles.styledAvatarOne}
+                      alt="Testv4"
+                      src="Testv4.jpeg"
+                    />
+                    <Avatar
+                      className={styles.styledAvatarTwo}
+                      sizes="small"
+                      alt="Faucet"
+                      src="ethereum.png"
+                    />
                   </Box>
-                </Box>
-                <Box className={styles.balanceInfoBox}>
-                  <Typography className={styles.balanceInfoTypography}>
-                    Your LP Token Balance:{" "}
-                    <Typography className={styles.balanceTypography}>
-                      {LPTokenBalance}
-                    </Typography>
-                  </Typography>
-                </Box>
 
-                <Box
-                  className={styles.buttonsBox}
-                  sx={{
-                    paddingRight: { xs: "10px", sm: "10px" },
-                    justifyContent: {
-                      xs: "space-between",
-                      sm: "flex-end",
-                      md: "flex-end",
-                    },
-                  }}
-                >
-                  {LPTokenBalance != 0 && (
-                    <button className={styles.removeLiquidityButton}>
-                      Remove Liquidity
-                    </button>
-                  )}
-                  <button
-                    className={styles.addLiquidityButton}
-                    onClick={() =>
-                      window.open(
-                        `https://app.balancer.fi/#/pool/${getPoolId(
-                          selectedNetwork
-                        )}/invest`,
-                        "_blank"
-                      )
-                    }
+                  <span
+                    className={styles.tokensTypography}
+                    sx={{ fontSize: { xs: 16 } }}
                   >
-                    Add Liquidity
-                  </button>
+                    {`${getPoolTokenSymbol(selectedNetwork)?.[0]} / ${
+                      getPoolTokenSymbol(selectedNetwork)?.[1]
+                    }`}
+                  </span>
+                  <span className={styles.feeTypography}>
+                    {getPoolFees(selectedNetwork)}
+                  </span>
                 </Box>
               </Box>
             </Box>
-          </Box>{" "}
-        </>
-      ) : (
-        <CircularProgressBar
-          margin={"20%"}
-          label={"Please Wait"}
-        ></CircularProgressBar>
-      )}
+            <Box className={styles.balanceInfoBox}>
+              <p className={styles.balanceInfoTypography}>
+                Your LP Token Balance:{" "}
+                {isWalletConnected ? (
+                  <span className={styles.balanceTypography}>
+                    {!loading ? LPTokenBalance : <Skeleton width={"100px"} />}
+                  </span>
+                ) : (
+                  <span className={styles.balanceTypography}>
+                    {isWalletConnected
+                      ? LPTokenBalance
+                      : "Connect your Wallet!"}
+                  </span>
+                )}
+              </p>
+            </Box>
+
+            <Box
+              className={styles.buttonsBox}
+              sx={{
+                paddingRight: { xs: "10px", sm: "10px" },
+                justifyContent: {
+                  xs: "space-between",
+                  sm: "flex-end",
+                  md: "flex-end",
+                },
+              }}
+            >
+              {LPTokenBalance != 0 && (
+                <button className={styles.removeLiquidityButton}>
+                  Remove Liquidity
+                </button>
+              )}
+              <button
+                className={styles.addLiquidityButton}
+                onClick={() =>
+                  window.open(
+                    `https://app.balancer.fi/#/pool/${getPoolId(
+                      selectedNetwork
+                    )}/invest`,
+                    "_blank"
+                  )
+                }
+              >
+                Add Liquidity
+              </button>
+            </Box>
+          </Box>
+        </Box>
+      </Box>{" "}
     </Box>
   );
 };
