dfb29e0514b2ffe2b8a0fa491a68b73645c1284c
Safal <mr.safaladh@gmail.com>
2023-03-08 16:40:26 +0545

    Converted constant.js to ts

---
+++ dex/src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const MAX_INPUT_LENGTH: number = 15;
+
+export const ORDER_STATUS_COMPLETED: string = "Completed";
+export const ORDER_STATUS_CANCELLED: string = "Cancelled";
+export const ORDER_STATUS_EXECUTED: string = "Execution Completed";
+export const ORDER_EXECUTION_TIME_REMAINING: string = "Time Remaining";
