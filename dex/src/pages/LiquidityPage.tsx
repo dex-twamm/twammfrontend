bbaf0a90cffa1f6565014ca12aa02634c198fc29
Safal <mr.safaladh@gmail.com>
2023-03-10 15:57:57 +0545

    converted LiquidityPage.jsx to tsx

--- dex/src/pages/LiquidityPage.tsx
+++ dex/src/pages/LiquidityPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LiquidityPools from "../components/Liquidity/LiquidityPools";
 
-const LiquidityPage = () => {
+const LiquidityPage: React.FC = () => {
   return (
     <div>
       <LiquidityPools />
