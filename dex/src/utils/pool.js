eec4037adca3899e38b1f41bb604a18eee1ef96a
Kunal Sangwan <kunalsangwan1@gmail.com>
2023-03-21 20:14:28 +0530

    Fix tokens ordering

--- dex/src/utils/pool.js
+++ dex/src/utils/pool.js
@@ -64,18 +64,18 @@ export const POOLS = {
       blockInterval: 100,
       tokens: [
         {
-          symbol: "USDC",
-          name: "USDC",
-          decimals: 6,
-          address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-          logo: usdLogo,
-        },
-        {
           symbol: "WBTC",
           name: "WBTC",
           decimals: 8,
           address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
           logo: wbtcLogo,
+        },
+        {
+          symbol: "USDC",
+          name: "USDC",
+          decimals: 6,
+          address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+          logo: usdLogo,
         },
       ],
     },
