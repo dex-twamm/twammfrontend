4221c8b32417904048ae9ffb72c0dc5f55af57be
Safal <mr.safaladh@gmail.com>
2023-03-23 17:59:10 +0545

    addPoolLiquidity to tsx

--- dex/src/utils/addPoolLiquidity.ts
+++ dex/src/utils/addPoolLiquidity.ts
@@ -1,19 +1,29 @@
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { defaultAbiCoder } from "ethers/lib/utils";
 import { MAX_UINT256 } from ".";
 import { VAULT_CONTRACT_ABI } from "../constants";
+import { SelectedNetworkType } from "../providers/context/NetworkProvider";
 import { getGasLimit } from "./getGasLimit";
 import { getVaultContractAddress } from "./networkUtils";
 
 export const addPoolLiquidity = async (
-  poolId,
-  tokenIn,
-  tokenOneAmountWei,
-  tokenTwoAmountWei,
-  walletAddress,
-  web3provider,
-  currentNetwork
+  poolId: string,
+  tokenIn: string[],
+  tokenOneAmountWei: BigNumber,
+  tokenTwoAmountWei: BigNumber,
+  walletAddress: string,
+  web3provider: any,
+  currentNetwork: SelectedNetworkType
 ) => {
+  console.log(
+    poolId,
+    tokenIn,
+    tokenOneAmountWei,
+    tokenTwoAmountWei,
+    walletAddress,
+    web3provider,
+    currentNetwork
+  );
   const encodedRequest = defaultAbiCoder.encode(
     ["uint256", "uint256[]", "uint256"],
     [2, [tokenOneAmountWei, tokenTwoAmountWei], 0]
