4214eedad6596cd9e8a3ff7ac8884c182293d6d3
Safal <mr.safaladh@gmail.com>
2023-03-23 17:52:51 +0545

    _withdrawPoolLiquidity to tsx

--- dex/src/utils/_withdrawPoolLiquidity.ts
+++ dex/src/utils/_withdrawPoolLiquidity.ts
@@ -1,17 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
 import { POPUP_MESSAGE } from "../constants";
+import { SelectedNetworkType } from "../providers/context/NetworkProvider";
 import { getPoolId, getPoolTokenAddresses } from "./poolUtils";
 import { withdrawPoolLiquidity } from "./withdrawPoolLiquidity";
 
 export const _withdrawPoolLiquidity = async (
-  walletAddress,
-  web3provider,
-  currentNetwork,
-  bptAmountIn,
-  setTransactionHash,
-  setMessage,
-  setLoading,
-  setError,
-  setShowPreviewModal
+  walletAddress: string,
+  web3provider: any,
+  currentNetwork: SelectedNetworkType,
+  bptAmountIn: number,
+  setTransactionHash: Dispatch<SetStateAction<string>>,
+  setMessage: Dispatch<SetStateAction<string>>,
+  setLoading: Dispatch<SetStateAction<boolean>>,
+  setError: Dispatch<SetStateAction<string>>,
+  setShowPreviewModal: Dispatch<SetStateAction<boolean>>
 ) => {
   try {
     const poolId = getPoolId(currentNetwork);
@@ -34,7 +36,7 @@ export const _withdrawPoolLiquidity = async (
           console.log("data after wait", result);
           return result;
         };
-        addLiquidityResult(res).then(async (response) => {
+        addLiquidityResult().then(async (response) => {
           if (response.status === 1) {
             setMessage(POPUP_MESSAGE.liquidityWithdrawn);
           } else setMessage(POPUP_MESSAGE.withdrawLiquidityFailed);
@@ -44,7 +46,7 @@ export const _withdrawPoolLiquidity = async (
         console.error(err);
         setMessage(POPUP_MESSAGE.addLiquidityFailed);
       })
-      .finally(setLoading(false));
+      .finally(() => setLoading(false));
   } catch (err) {
     console.error(err);
     setLoading(false);
