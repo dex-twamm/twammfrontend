ed429e50c193bc12980a48c389eebd121ae47b3b
Safal <mr.safaladh@gmail.com>
2023-05-04 12:18:52 +0545

    Discord and twitter linked

--- dex/src/components/landingPage/LandingFooter.tsx
+++ dex/src/components/landingPage/LandingFooter.tsx
@@ -1,9 +1,16 @@
 import styles from "../../css/LandingPage.module.css";
 import discord from "../../images/discord.svg";
 import twitter from "../../images/twitter.svg";
-import facebook from "../../images/facebook.svg";
+// import facebook from "../../images/facebook.svg";
 
 const LandingFooter = () => {
+  const handleSocialMedia = (socialMedia: string) => {
+    if (socialMedia === "twitter") {
+      window.open("https://twitter.com/FinanceXS", "_blank");
+    } else {
+      window.open("https://discord.gg/N2rbZwCNhq", "_blank");
+    }
+  };
   return (
     <div className={styles.footerSection}>
       <div className={styles.leftSec}>
@@ -14,9 +21,19 @@ const LandingFooter = () => {
       <div className={styles.rightSec}>
         <p>Connect with us:</p>
         <div className={styles.socialMedia}>
-          <img className={styles.discord} src={discord} alt="img" />
-          <img className={styles.twitter} src={twitter} alt="img" />
-          <img className={styles.faceBook} src={facebook} alt="img" />
+          <img
+            className={styles.discord}
+            onClick={() => handleSocialMedia("discord")}
+            src={discord}
+            alt="img"
+          />
+          <img
+            className={styles.twitter}
+            onClick={() => handleSocialMedia("twitter")}
+            src={twitter}
+            alt="img"
+          />
+          {/* <img className={styles.faceBook} src={facebook} alt="img" /> */}
         </div>
       </div>
     </div>
