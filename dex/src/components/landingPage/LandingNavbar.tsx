06fe08efda9aa0be2238ad03343857f41b3b81e8
Safal <mr.safaladh@gmail.com>
2023-05-04 12:06:42 +0545

    Longswap docs linked to Navbar docs

--- dex/src/components/landingPage/LandingNavbar.tsx
+++ dex/src/components/landingPage/LandingNavbar.tsx
@@ -2,6 +2,9 @@ import styles from "../../css/LandingPage.module.css";
 import logo from "../../images/logo.png";
 
 const LandingNavbar = () => {
+  const handleDocsClick = () => {
+    window.open("https://docs.longswap.xyz/", "_blank");
+  };
   return (
     <div className={styles.navContainer}>
       <div className={styles.logoAndImage}>
@@ -9,7 +12,9 @@ const LandingNavbar = () => {
         <p className={styles.longSwap}>Longswap</p>
       </div>
       <div className={styles.docAndCommunity}>
-        <p className={styles.docs}>Docs</p>
+        <p className={styles.docs} onClick={handleDocsClick}>
+          Docs
+        </p>
         <button className={styles.community}>Join the community</button>
       </div>
     </div>
